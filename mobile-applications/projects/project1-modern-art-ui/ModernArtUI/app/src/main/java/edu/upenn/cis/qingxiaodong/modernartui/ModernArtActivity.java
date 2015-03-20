53c4b750ad2a35e02f79c35c96cda55cd6901be6
Qingxiao Dong <qingxiao.dong@gmail.com>
2015-03-20 15:05:06 -0400

    Completed course project.

--- mobile-applications/projects/project1-modern-art-ui/ModernArtUI/app/src/main/java/edu/upenn/cis/qingxiaodong/modernartui/ModernArtActivity.java
+++ mobile-applications/projects/project1-modern-art-ui/ModernArtUI/app/src/main/java/edu/upenn/cis/qingxiaodong/modernartui/ModernArtActivity.java
@@ -1,8 +1,8 @@
 package edu.upenn.cis.qingxiaodong.modernartui;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -11,17 +11,44 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 
 public class ModernArtActivity extends ActionBarActivity {
 
-    private DialogFragment mDialog;
     static private final String URL = "http://www.moma.org";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modern_art);
+        SeekBar colorChanger = (SeekBar) findViewById(R.id.seekBar);
+        colorChanger.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progressValue = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ((ImageView) findViewById(R.id.color1)).setBackgroundColor(Color.argb(0xFF,
+                        233+22*progress/100, 30+163*progress/100, 99-92*progress/100));
+                ((ImageView) findViewById(R.id.color2)).setBackgroundColor(Color.argb(0xFF,
+                        63+(244-63)*progress/100, 81+(255-81)*progress/100, 181-(181-129)*progress/100));
+                ((ImageView) findViewById(R.id.color3)).setBackgroundColor(Color.argb(0xFF,
+                        255, 235+(249-235)*progress/100, 59+(196-59)*progress/100));
+                ((ImageView) findViewById(R.id.color5)).setBackgroundColor(Color.argb(0xFF,
+                        183+(139-183)*progress/100, 28+(195-28)*progress/100, 28+(74-28)*progress/100));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
